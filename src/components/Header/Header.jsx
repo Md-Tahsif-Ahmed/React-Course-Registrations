328d201932add8ce3a3e93808f6028f562b5a6da
tahsif <tahsif.cse@gmail.com>
2023-09-21 07:48:06 +0600

    Create Heading of Course Registration

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+ const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-semibold text-black text-center my-12'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
